ca3764fcbe6ad760070dd7fcc32dac2363e527ae
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-05 20:24:32 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -59,7 +59,6 @@ class Translation(object):
         InlineKeyboardButton('⛔ Close', callback_data='close')
         ]]
     )
-
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
