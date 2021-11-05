8497aa46a11891f05240335820eed8b916f1b3a4
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-05 18:53:29 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -26,10 +26,15 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from pyrogram.types.bots_and_keyboards import InlineKeyboardButton, InlineKeyboardMarkup
 
 
+
+
 @pyrogram.Client.on_message(pyrogram.filters.command(["start"]))
 async def start(bot, update):
-    await update.reply_text(
-        text=Translation.START_TEXT.format(update.from_user.mention),
-        disable_web_page_preview=True,
-        reply_markup=Translation.START_BUTTONS
-    )
+    if update.from_user.id in Config.AUTH_USERS:
+        # logger.info(update)
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.START_TEXT.format(update.from_user.first_name),
+            reply_markup=Translation.START_BUTTONS
+            reply_to_message_id=update.message_id
+        )
